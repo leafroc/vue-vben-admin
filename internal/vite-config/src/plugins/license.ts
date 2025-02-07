2b9df38dce4c7a2d27eb031b47cb3a735e66a3d2
leafroc <yp324ja@sina.com>
2025-02-07 10:20:22 +0800

    调整版权信息

--- internal/vite-config/src/plugins/license.ts
+++ internal/vite-config/src/plugins/license.ts
@@ -33,7 +33,7 @@ async function viteLicensePlugin(
   * Vben Admin
   * Version: ${version}
   * Author: vben
-  * Copyright (C) 2024 Vben
+  * Copyright (C) 2025 点滴信息
   * License: MIT License
   * Description: ${description}
   * Date Created: ${date}
